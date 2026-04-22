e52bd855993f6f172f73106cdd9c7e089650b6f6
Joseeeee000 <josehenrique7418529630@gmail.com>
2026-04-22 13:55:58 -0300

    22.04.2026
    opa

---
+++ aula-1/FASES/area_2d.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+
+@export var prox_fase = ""
+
+
+func _on_body_entered(_body: Node2D) -> void:
+
+	call_deferred("load_next_scene")
+
+	
+
+func load_next_scene():
+
+	get_tree().change_scene_to_file("res://FASES/" + prox_fase + ".tscn")
